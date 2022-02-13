fa0e12683a757b80cd3d4995d222186607227ed6
Cristina Lester <cristina.deelester@gmail.com>
2022-02-12 18:09:25 -0800

    added patterns

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 import Square from "./components/Square";
+import PATTERNS from "./Patterns";
 
 function App() {
   //using a matrix as a board
