fb556e3f37f8862f03b30d33b3101cfb32422b37
Cristina Lester <cristina.deelester@gmail.com>
2022-02-12 18:49:10 -0800

    added checkWinner function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Square from "./components/Square";
 import PATTERNS from "./Patterns";
 
@@ -13,19 +13,47 @@ function App() {
   ]);
 
   const [player, setPlayer] = useState("❌");
+  const [result, setResult] = useState({ winner: "none", status: "none" });
 
   const chooseSlot = (i, j) => {
     let updatedBoard = [...board];
-    if (board[i][j] === "") {
+    if (updatedBoard[i][j] === "") {
       updatedBoard[i][j] = player;
       setBoard(updatedBoard);
-      nextTurn(player);
     }
   };
 
   const nextTurn = (currentPlayer) => {
     setPlayer(currentPlayer === "❌" ? "⭕" : "❌");
   };
+
+  const checkWinner = () => {
+    PATTERNS.forEach((configuration) => {
+      let i = configuration[0][0];
+      let j = configuration[0][1];
+      let slot = board[i][j];
+      if (slot === "") return false;
+      let fonudWinningPatter = true;
+
+      configuration.forEach((couple) => {
+        let x = couple[0];
+        let y = couple[1];
+        if (board[x][y] !== slot) fonudWinningPatter = false;
+      });
+
+      if (fonudWinningPatter) {
+        setResult({
+          winner: player,
+          status: "Won",
+        });
+        return true;
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (!checkWinner()) nextTurn(player);
+  }, [board]);
 
   return (
     <div className="App">
